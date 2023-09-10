ee16a613d80c9cfc68c770dca00062357dbeeb5b
Willian <willianbylla@hotmail.com>
2023-09-10 08:33:05 -0300

    test: add test to LocalSaveFipeInfo

---
+++ test/data/mocks/cache_storage_mock.dart
@@ -0,0 +1,13 @@
+import 'package:mocktail/mocktail.dart';
+
+import 'package:mobcar/data/cache/cache.dart';
+
+class CacheStorageMock extends Mock implements CacheStorage {
+  CacheStorageMock() {
+    mockSave();
+  }
+
+  When mockSaveCall() => when(() => save(key: any(named: 'key'), value: any(named: 'value')));
+  void mockSave() => mockSaveCall().thenAnswer((_) async => _);
+  void mockSaveError() => mockSaveCall().thenThrow(Exception());
+}
--- test/data/mocks/mocks.dart
+++ test/data/mocks/mocks.dart
@@ -1,1 +1,2 @@
 export 'http_client_mock.dart';
+export 'cache_storage_mock.dart';
